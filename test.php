1ffea1fa9a224076f65fd47cb207b7ec0789091e
wensjuma <wensjuma@gmail.com>
2019-03-16 23:54:01 +0300

    [Starts #20190316] Initial Commit to Ecom system

---
+++ test.php
@@ -0,0 +1,99 @@
+<?php
+$ip_add = getenv("REMOTE_ADDR");
+include "db.php";
+include "action.php";
+$today = date("Ymd");
+$rand = strtoupper(substr(uniqid(sha1(time())),0,4));
+$unique = $today . $rand;
+//$sql = "INSERT INTO `orders`( `user_id`, `product_id`, `qty`, `trx_id`, `p_status`) VALUES ('4','6','7','$unique','tyrytr')";
+			//	mysqli_query($con,$sql);
+
+		
+
+				$trx_id = $unique /*$_GET["tx"]*/;
+		$p_st = 'Completed' /*$_GET["st"]*/;
+		$amt = $_GET["amt"];
+		// $cc = $_GET["cc"];
+		$cm_user_id = $_SESSION["uid"] /*$_GET["cm"]*/;
+		// $c_amt = $_COOKIE["ta"];
+	 ($p_st == "Completed");
+
+		$sql = "SELECT p_id,qty FROM cart WHERE user_id = '$cm_user_id'";
+		$query = mysqli_query($con,$sql);
+		if (mysqli_num_rows($query) > 0) {
+			# code...
+			while ($row=mysqli_fetch_array($query)) {
+			$product_id[] = $row["p_id"];
+			$qty[] = $row["qty"];
+			}
+
+			for ($i=0; $i < count($product_id); $i++) { 
+				$sql = "INSERT INTO orders (user_id,product_id,qty,trx_id,p_status) VALUES ('$cm_user_id','".$product_id[$i]."','".$qty[$i]."','$trx_id','$p_st')";
+				mysqli_query($con,$sql);
+			}
+
+			$sql = "DELETE FROM cart WHERE user_id = '$cm_user_id'";
+			mysqli_query($con,$sql);
+			}
+
+
+				//echo "seccess";
+				//$userz = $_SESSION["name"];
+
+//header("location:testsucces.php");
+
+?>
+
+</html>
+<!DOCTYPE html>
+					<html>
+						<head>
+							<meta charset="UTF-8">
+							<title>PyTeam</title>
+							<link rel="stylesheet" href="css/bootstrap.min.css"/>
+							<script src="js/jquery2.js"></script>
+							<script src="js/bootstrap.min.js"></script>
+							<script src="main.js"></script>
+							<style>
+								table tr td {padding:10px;}
+							</style>
+						</head>
+					<body>
+						<div class="navbar navbar-inverse navbar-fixed-top">
+							<div class="container-fluid">	
+								<div class="navbar-header">
+									<a href="#" class="navbar-brand">PyTeam</a>
+								</div>
+								<ul class="nav navbar-nav">
+									<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+									<li><a href="profile.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+								</ul>
+							</div>
+						</div>
+						<p><br/></p>
+						<p><br/></p>
+						<p><br/></p>
+						<div class="container-fluid">
+						
+							<div class="row">
+								<div class="col-md-2"></div>
+								<div class="col-md-8">
+									<div class="panel panel-default">
+										<div class="panel-heading"></div>
+										<div class="panel-body">
+											<h1>Thankyou </h1>
+											<hr/>
+											<p>Hello <?php echo "<b>".$_SESSION["name"]."</b>"; ?>,Your payment process is 
+											successfully completed and your Transaction id is <b><?php echo $trx_id; ?></b><br/>
+											you can continue your Shopping <br/></p>
+											<a href="index.php" class="btn btn-success btn-lg">Continue Shopping</a>
+										</div>
+										<div class="panel-footer"></div>
+									</div>
+								</div>
+								<div class="col-md-2"></div>
+							</div>
+						</div>
+					</body>
+					</html>
+
---
+++ utils.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+if(isset($_SESSION["uid"])){
+	header("location:profile.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PyTeam</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<style></style>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+
+	<nav class="navbar navbar-inverse ">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">PyTeam</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+			</ul>
+			<form class="navbar-form navbar-left">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search" id="search">
+		        </div>
+		        <button type="submit" class="btn btn-primary" id="search_btn"><span class="glyphicon glyphicon-search"></span></button>
+		     </form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span> Cart <span class="badge">0</span></a>
+					<div class="dropdown-menu " style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in Ksh.</div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div  id="cart_product">
+								<!--<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in $.</div>
+								</div>-->
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-log-in"></span> Login </a>
+					<ul class="dropdown-menu">
+						
+							<div class="panel panel-primary" style="width:300px;">
+								<div class="panel-heading">Login</div>
+								<div class="panel-body">
+									<form onsubmit="return false" id="login">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" name="email" id="email" required/>
+										<label for="email">Password</label>
+										<input type="password" class="form-control" name="password" id="password" required/>
+										<p></p>
+										<a href="#">Forgotten Password</a>
+										<input type="submit" class="btn btn-success" style="float:right;">
+									</form>
+								</div>
+								<div class="panel-footer" id="e_msg"></div>
+					
+						</div>
+					</ul>
+				</li>
+				<!-- <li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up </a></li>
+				</li> -->
+
+			</ul>
+		</div>
+
+	</div>
+</nav>
+</body>
+</html>
